cabbce334f24c26340f47c63a09c4e9e368e4e0c
Anna FitzGerald <46199551+AEFitzGerald@users.noreply.github.com>
2021-05-23 16:26:40 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+// when user clicks button function is triggered
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    //console.log("yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, Come Back!";
+    button.classList.add("disappear");
+  } else {
+    //console.log("no");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
+});
